c17929d2ee431f575986c7aeb56b855561aa235b
sarah03qc <prisilla2003@gmail.com>
2022-11-23 19:32:11 -0600

    ahora si

--- java&cpp/Receiver.h
+++ java&cpp/Receiver.h
@@ -25,8 +25,11 @@ class Receiver {
             vector<string> elems;
             stringstream ss(fullinfo);
             
-            sregex_token_iterator iter(fullinfo.begin(), fullinfo.end(), re, -1);
-            sregex_token_iterator end;
+            while (ss.good()) {
+                string substr;
+                getline(ss, substr, ',');
+                elems.push_back(substr);
+            }
             
             for(int i = 0; i < elems.size(); i++) {
                 cout << elems[i] << endl;
@@ -35,12 +38,8 @@ class Receiver {
             nombre = elems[0];
             pass1 = elems[1];
             pass2 = elems[2];
-<<<<<<< HEAD
             descrDem = elems[3];
             descrOffer = elems[4];       
-=======
-      
->>>>>>> cb7c5d8ea0305d2ec1e0c56e9e12341f8c7fde7d
 
 
             //se genera el perfil con la informacion sacada de los sockets
@@ -48,7 +47,7 @@ class Receiver {
             cout << "nombre " << nombre << endl;
             cout << "pass1 " << pass1 << endl;
             cout << "pass2 " << pass2 << endl;
-            cout << "descrOfer " << descrOfer << endl;
+            cout << "descrOffer " << descrOffer << endl;
             cout << "descrDem " << descrDem << endl;
             cout << "descrOffer " << descrOffer << endl;
 
